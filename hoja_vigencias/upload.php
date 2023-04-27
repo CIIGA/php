30598452752e63acff88f10c11e2d03d2af71bdf
CIIGA <ciigasistemas@gmail.com>
2023-04-27 17:03:01 -0500

    vigencias4

--- hoja_vigencias/upload.php
+++ hoja_vigencias/upload.php
@@ -17,35 +17,35 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         $data_array =  $spreadsheet->toArray();
         
 
-        if(count($data_array) > 0) {
-           
-            foreach($data_array as $datos) {
-                if(!is_string($datos[0]) && $datos[0] != "") {
-                    $query = '';
-                    $query = 'INSERT INTO prueba_vigencias (cuenta, [FECHA INICIO], [FECHA FINAL]) VALUES';
-                    $FechaI = ($datos[1] == '') ? '' : date("Y-m-d", strtotime(str_replace('/', "-", $datos[1])));
-                    $FechaF = ($datos[2] == '') ? '' : date("Y-m-d", strtotime(str_replace('/', "-", $datos[2])));
+        // if(count($data_array) > 0) {
+        //     sqlsrv_query($cnx, "TRUNCATE TABLE prueba_vigencias;");
+        //     foreach($data_array as $datos) {
+        //         if(!is_string($datos[0]) && $datos[0] != "") {
+        //             $query = '';
+        //             $query = 'INSERT INTO prueba_vigencias (cuenta, [FECHA INICIO], [FECHA FINAL]) VALUES';
+        //             $FechaI = ($datos[1] == '') ? '' : date("Y-m-d", strtotime(str_replace('/', "-", $datos[1])));
+        //             $FechaF = ($datos[2] == '') ? '' : date("Y-m-d", strtotime(str_replace('/', "-", $datos[2])));
                     
-                    $query .= "('" . $datos[0] . "',";
-                    $query .= "'" . $FechaI . "',";
-                    $query .= "'" . $FechaF . "'), ";
-                    $query = substr($query, 0, strlen($query) - 2);
-            $query .= ";";
+        //             $query .= "('" . $datos[0] . "',";
+        //             $query .= "'" . $FechaI . "',";
+        //             $query .= "'" . $FechaF . "'), ";
+        //             $query = substr($query, 0, strlen($query) - 2);
+        //     $query .= ";";
 
             
 
-            sqlsrv_query($cnx, "TRUNCATE TABLE prueba_vigencias;");
-            sqlsrv_query($cnx, $query) or die(print_r(sqlsrv_errors()));
+            
+        //     sqlsrv_query($cnx, $query) or die(print_r(sqlsrv_errors()));
            
                     
-                }
-            }
-        }
+        //         }
+        //     }
+        // }
         
         sqlsrv_close($cnx);
             
 
-            header('Location: ../map.php?error=0&msg=Datos Guardados & plz='.$plz);
+            // header('Location: ../map.php?error=0&msg=Datos Guardados & plz='.$plz);
         
     } else {
         header('Location: ../map.php?error=1&msg=El archivo no es correcto & plz='.$plz);
