6ae3f6492f35d45a6f1d62047d5238970990100b
CIIGA <ciigasistemas@gmail.com>
2023-05-03 17:43:05 -0600

    max time

--- Reporte/txt/pregrabadas.php
+++ Reporte/txt/pregrabadas.php
@@ -1,4 +1,5 @@
 <?php
+ini_set('max_execution_time', 0);
 require '../modules/pregrabadas.php';
 //Se condiciona si se reciben como paarametro lo siguiente
 if ((isset($_GET['base'])) && (isset($_GET['fecha_inicial'])) && (isset($_GET['fecha_final']))) {
